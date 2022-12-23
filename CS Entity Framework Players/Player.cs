fb3498bc72a40bde66d0bd42de34e79f50ba3089
Thomas Youssef <thomasyoussef.business@protonmail.com>
2022-12-23 01:15:46 +0100

    Add project files.

---
+++ CS Entity Framework Players/Player.cs
@@ -0,0 +1,23 @@
+﻿namespace CS_Entity_Framework_Players {
+    public class Player {
+		// CAMPI
+
+
+		// PROPRIETÀ
+		public int PlayerId { get; set; }
+		public string Name { get; set; }
+		public string Surname { get; set; }
+		public int Score { get; set; }
+		public int GamesPlayed { get; set; }
+		public int GamesWon { get; set; }
+
+		// RELAZIONI
+
+
+		// METODI PUBBLICI
+
+
+		// METODI PRIVATI
+
+	}
+}
---
+++ CS Entity Framework Players/Program.cs
@@ -0,0 +1,5 @@
+﻿global using Microsoft.EntityFrameworkCore;
+global using System.ComponentModel.DataAnnotations;
+global using System.ComponentModel.DataAnnotations.Schema;
+
+Console.WriteLine("Hello, World!");
